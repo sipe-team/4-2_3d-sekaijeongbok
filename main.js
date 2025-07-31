856a012ef702b3858e334401b779bd76ad28009e
JinYeong Park <jypark@pcninc.co.kr>
2025-07-31 10:57:35 +0900

    0731 commit changes

--- main.js
+++ main.js
@@ -9,9 +9,36 @@ import { LuminosityShader } from 'three/addons/shaders/LuminosityShader.js';
 import { SobelOperatorShader } from 'three/addons/shaders/SobelOperatorShader.js';
 import GUI from 'three/examples/jsm/libs/lil-gui.module.min.js';
 
+import { TTFLoader } from 'three/addons/loaders/TTFLoader.js';
+import { Font } from 'three/addons/loaders/FontLoader.js';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
+
 
 let camera, scene, renderer, composer;
 let effectSobel;
+let container;
+let group, textMesh1, textMesh2, textGeom, material;
+let firstLetter = true;
+
+let text = 'three.js';
+const depth = 20,
+    size = 70,
+    hover = 30,
+    curveSegments = 4,
+    bevelThickness = 2,
+    bevelSize = 1.5;
+
+let font = null;
+const mirror = true;
+
+let targetRotation = 0;
+let targetRotationOnPointerDown = 0;
+
+let pointerX = 0;
+let pointerXOnPointerDown = 0;
+
+let windowHalfX = window.innerWidth / 2;
+
 const params = { 
     enable: true
 };
@@ -19,128 +46,290 @@ const params = {
 init();
 
 function init(){
-            //scene 객체 생성
-            scene = new THREE.Scene();
-            //scene.background = new THREE.Color('white');
-            //camera 객체 생성;
-            const camera = new THREE.PerspectiveCamera(
-                60, // field of view (시야각)
-                window.innerWidth/window.innerHeight, // aspect ratio(가로 세로 길이의 비율)
-                0.1, // near (근거리 클리핑 평면)
-                1000 // far (원거리 클리핑 평면)
-            );
-            //camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 100 );
-            camera.position.set( 0, 1, 5 );
-            camera.lookAt( scene.position );
+    container = document.createElement('div');
+    document.body.appendChild(container);
 
-            const geometry = new THREE.TorusKnotGeometry(1, 0.3, 256, 32);
-            const material = new THREE.MeshPhongMaterial( { color: 0xffff00 } );
+    //scene 객체 생성
+    scene = new THREE.Scene();
+    //scene.background = new THREE.Color('white');
+    //camera 객체 생성;
+    const camera = new THREE.PerspectiveCamera(
+        40, // field of view (시야각)
+        window.innerWidth/window.innerHeight, // aspect ratio(가로 세로 길이의 비율)
+        0.1, // near (근거리 클리핑 평면)
+        1000 // far (원거리 클리핑 평면)
+    );
+    //camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 100 );
+    camera.position.set( 0, 2, 10 );
+    camera.lookAt( scene.position );
 
-            const mesh = new THREE.Mesh( geometry, material );
-            mesh.position.set(0, 0, -4);
-            scene.add( mesh );
+    const geometry = new THREE.TorusKnotGeometry(1, 0.3, 256, 32);
+    const material = new THREE.MeshPhongMaterial( { color: 0xffff00 } );
 
-            //
+    const mesh = new THREE.Mesh( geometry, material );
+    mesh.position.set(0, 0, -4);
+    scene.add( mesh );
 
-            const ambientLight = new THREE.AmbientLight( 0xe7e7e7 );
-            scene.add( ambientLight );
+    //
 
-            const pointLight = new THREE.PointLight( 0xffffff, 20 );
-            camera.add( pointLight );
-            scene.add( camera );
+    const ambientLight = new THREE.AmbientLight( 0xe7e7e7 );
+    scene.add( ambientLight );
 
-            //
+    const pointLight = new THREE.PointLight( 0xffffff, 20 );
+    camera.add( pointLight );
+    scene.add( camera );
 
-            renderer = new THREE.WebGLRenderer();
-            renderer.setPixelRatio( window.devicePixelRatio );
-            renderer.setSize( window.innerWidth, window.innerHeight );
-            renderer.setAnimationLoop( animate );
-            document.body.appendChild( renderer.domElement );
+    //
+    group = new THREE.Group();
+    group.position.y = 100;
 
-            const color = 0xFFFFFF;
-            const intensity = 1;
-            const light = new THREE.DirectionalLight(color,intensity);
-            light.position.set(0,10,0);
-            light.target.position.set(-5, 0, 0);
-            scene.add(light);
-            scene.add(light.target);
+    scene.add( group );
 
-            // 3D 모델 로딩용 GLTF Loader 객체 생성
-            const loader = new GLTFLoader();
-            loader.load('Resouce/3D/sipe_arrow.glb', function(gltf){
-             
-            scene.add(gltf.scene);
-            }, undefined, function(error){
-                console.error(error);
-            });
-            //Web Graphics Library 웹 상에서 2D 및 3D 그래픽 렌더링을 위한 로우레벨 javaScript API
-           // const renderer = new THREE.WebGLRenderer({
-            //    canvas : document.querySelector("#canvas"),
-            //    antialias : true
-            //});
+    const tloader = new TTFLoader();
 
-        // postprocessing
+    tloader.load( 'fonts/ttf/kenpixel.ttf', function ( json ) {
 
-				composer = new EffectComposer( renderer );
-				const renderPass = new RenderPass( scene, camera );
-				composer.addPass( renderPass );
+        font = new Font( json );
+        createText();
 
-				// color to grayscale conversion
+    } );
 
-				const effectGrayScale = new ShaderPass( LuminosityShader );
-				composer.addPass( effectGrayScale );
+    const plane = new THREE.Mesh(
+        new THREE.PlaneGeometry( 10000, 10000 ),
+        new THREE.MeshBasicMaterial( { color: 0xffffff, opacity: 0.5, transparent: true } )
+    );
+    plane.position.y = 100;
+    plane.rotation.x = - Math.PI / 2;
+    scene.add( plane );
+    //
 
-				// you might want to use a gaussian blur filter before
-				// the next pass to improve the result of the Sobel operator
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setAnimationLoop( animate );
+    document.body.appendChild( renderer.domElement );
 
-				// Sobel operator
+    const color = 0xFFFFFF;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color,intensity);
+    light.position.set(0,10,0);
+    light.target.position.set(-5, 0, 0);
+    scene.add(light);
+    scene.add(light.target);
 
-				effectSobel = new ShaderPass( SobelOperatorShader );
-				effectSobel.uniforms[ 'resolution' ].value.x = window.innerWidth * window.devicePixelRatio;
-				effectSobel.uniforms[ 'resolution' ].value.y = window.innerHeight * window.devicePixelRatio;
-				composer.addPass( effectSobel );
+    // 3D 모델 로딩용 GLTF Loader 객체 생성
+    const gloader = new GLTFLoader();
+    gloader.load('Resouce/3D/sipe_arrow.glb', function(gltf){
+    scene.add(gltf.scene);
+    }, undefined, function(error){
+        console.error(error);
+    });
+    //Web Graphics Library 웹 상에서 2D 및 3D 그래픽 렌더링을 위한 로우레벨 javaScript API
+    // const renderer = new THREE.WebGLRenderer({
+    //    canvas : document.querySelector("#canvas"),
+    //    antialias : true
+    //});
 
-				const controls = new OrbitControls( camera, renderer.domElement );
-				controls.enableZoom = false;
+// postprocessing
+
+    composer = new EffectComposer( renderer );
+    const renderPass = new RenderPass( scene, camera );
+    composer.addPass( renderPass );
+
+    // color to grayscale conversion
+
+    const effectGrayScale = new ShaderPass( LuminosityShader );
+    composer.addPass( effectGrayScale );
+
+    // you might want to use a gaussian blur filter before
+    // the next pass to improve the result of the Sobel operator
+
+    // Sobel operator
+
+    effectSobel = new ShaderPass( SobelOperatorShader );
+    effectSobel.uniforms[ 'resolution' ].value.x = window.innerWidth * window.devicePixelRatio;
+    effectSobel.uniforms[ 'resolution' ].value.y = window.innerHeight * window.devicePixelRatio;
+    composer.addPass( effectSobel );
+
+    const controls = new OrbitControls( camera, renderer.domElement );
+    controls.enableZoom = true;
+
+    //
+
+    const gui = new GUI();
+
+    gui.add( params, 'enable' );
+    gui.open();
 
 				//
+    container.style.touchAction = 'none';
+    container.addEventListener( 'pointerdown', onPointerDown );
 
-				const gui = new GUI();
-
-				gui.add( params, 'enable' );
-				gui.open();
-
-				//
-
-				window.addEventListener( 'resize', onWindowResize );
+    document.addEventListener( 'keypress', onDocumentKeyPress );
+    document.addEventListener( 'keydown', onDocumentKeyDown );
+    window.addEventListener( 'resize', onWindowResize );
 }
 
 function onWindowResize() {
 
-            //camera. = window.innerWidth / window.innerHeight;
-            camera.updateProjectionMatrix();
+    //camera. = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 
-            renderer.setSize( window.innerWidth, window.innerHeight );
-            composer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    composer.setSize( window.innerWidth, window.innerHeight );
 
-            effectSobel.uniforms[ 'resolution' ].value.x = window.innerWidth * window.devicePixelRatio;
-            effectSobel.uniforms[ 'resolution' ].value.y = window.innerHeight * window.devicePixelRatio;
+    effectSobel.uniforms[ 'resolution' ].value.x = window.innerWidth * window.devicePixelRatio;
+    effectSobel.uniforms[ 'resolution' ].value.y = window.innerHeight * window.devicePixelRatio;
 
-        }
+}
+
+function onDocumentKeyDown( event ) {
+
+    if ( firstLetter ) {
+
+        firstLetter = false;
+        text = '';
+
+    }
+
+    const keyCode = event.keyCode;
+
+    // backspace
+
+    if ( keyCode === 8 ) {
+
+        event.preventDefault();
+
+        text = text.substring( 0, text.length - 1 );
+        refreshText();
+
+        return false;
+
+    }
+
+}
+
+function onDocumentKeyPress( event ) {
+
+    const keyCode = event.which;
+
+    // backspace
+
+    if ( keyCode === 8 ) {
+
+        event.preventDefault();
+
+    } else {
+
+        const ch = String.fromCharCode( keyCode );
+        text += ch;
+
+        refreshText();
+
+    }
+
+}
+
+function createText() {
+
+    textGeo = new TextGeometry( text, {
+
+        font: font,
+
+        size: size,
+        depth: depth,
+        curveSegments: curveSegments,
+
+        bevelThickness: bevelThickness,
+        bevelSize: bevelSize,
+        bevelEnabled: true
+
+    } );
+
+    textGeo.computeBoundingBox();
+    textGeo.computeVertexNormals();
+
+    const centerOffset = - 0.5 * ( textGeo.boundingBox.max.x - textGeo.boundingBox.min.x );
+
+    textMesh1 = new THREE.Mesh( textGeo, material );
+
+    textMesh1.position.x = centerOffset;
+    textMesh1.position.y = hover;
+    textMesh1.position.z = 0;
+
+    textMesh1.rotation.x = 0;
+    textMesh1.rotation.y = Math.PI * 2;
+
+    group.add( textMesh1 );
+
+    if ( mirror ) {
+
+        textMesh2 = new THREE.Mesh( textGeo, material );
+
+        textMesh2.position.x = centerOffset;
+        textMesh2.position.y = - hover;
+        textMesh2.position.z = depth;
+
+        textMesh2.rotation.x = Math.PI;
+        textMesh2.rotation.y = Math.PI * 2;
+
+        group.add( textMesh2 );
+
+    }
+
+}
+
+function refreshText() {
+
+    group.remove( textMesh1 );
+    if ( mirror ) group.remove( textMesh2 );
+
+    if ( ! text ) return;
+
+    createText();
+
+}
+
+function onPointerDown( event ) {
+
+    if ( event.isPrimary === false ) return;
+
+    pointerXOnPointerDown = event.clientX - windowHalfX;
+    targetRotationOnPointerDown = targetRotation;
+
+    document.addEventListener( 'pointermove', onPointerMove );
+    document.addEventListener( 'pointerup', onPointerUp );
+
+}
+
+function onPointerMove( event ) {
+
+    if ( event.isPrimary === false ) return;
+
+    pointerX = event.clientX - windowHalfX;
+
+    targetRotation = targetRotationOnPointerDown + ( pointerX - pointerXOnPointerDown ) * 0.02;
+
+}
+
+function onPointerUp( event ) {
+
+    if ( event.isPrimary === false ) return;
+
+    document.removeEventListener( 'pointermove', onPointerMove );
+    document.removeEventListener( 'pointerup', onPointerUp );
+
+}
 
 function animate() {
 
-            if ( params.enable === true ) {
-
-                composer.render();
-
-            } else {
-
-                renderer.render( scene, camera );
-
-            }
-
+    if ( params.enable === true ) {
+        composer.render();
+    } 
+    else {
+        renderer.render( scene, camera );
     }
+}
 //renderer.outputColorSpace = THREE.SRGBColorSpace;
 //renderer.setSize(window.innerWidth, window.innerHeight);
 //document.body.appendChild(renderer.domElement);
