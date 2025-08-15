88cd4b96630fccf117276bff45c2791921c504fc
y09n <euihyun.yang.9x@gmail.com>
2025-08-16 00:21:35 +0900

    Update euihyun/src/routes/monument.tsx

--- euihyun/src/routes/monument.tsx
+++ euihyun/src/routes/monument.tsx
@@ -1,46 +1,23 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { createFileRoute } from "@tanstack/react-router";
+import type { ComponentProps } from "react";
 
 export const Route = createFileRoute("/monument")({
   component: () => <MonumentValley />,
 });
 
-function AxisHelper() {
-  return (
-    <group position={[0, 0, 0]}>
-      {/* X */}
-      <mesh position={[1, 0, 0]} rotation={[0, 0, Math.PI / 2]}>
-        <cylinderGeometry args={[0.02, 0.02, 2]} />
-        <meshLambertMaterial color="#FF0000" />
-      </mesh>
-
-      {/* Y */}
-      <mesh position={[0, 1, 0]}>
-        <cylinderGeometry args={[0.02, 0.02, 2]} />
-        <meshLambertMaterial color="#00FF00" />
-      </mesh>
-
-      {/* Z */}
-      <mesh position={[0, 0, 1]} rotation={[Math.PI / 2, 0, 0]}>
-        <cylinderGeometry args={[0.02, 0.02, 2]} />
-        <meshLambertMaterial color="#0000FF" />
-      </mesh>
-    </group>
-  );
-}
-
 function MonumentValley() {
   return (
     <div className="w-full h-screen">
-      <Canvas camera={{ position: [6, 4, 6], fov: 45 }} shadows>
+      <Canvas camera={{ position: [6, 4, 6], fov: 25 }} shadows>
         <color attach="background" args={["#2A3B47"]} />
 
         <ambientLight intensity={0.2} color="#E0E6ED" />
         <directionalLight
           position={[5, 8, 5]}
           intensity={0.6}
-          color="#FFFFFF"
+          color="#EEE"
           castShadow
           shadow-mapSize={[2048, 2048]}
           shadow-camera-far={50}
@@ -56,71 +33,92 @@ function MonumentValley() {
           distance={8}
         />
 
-        {/* Floor */}
         <group position={[0, 0, 0]}>
-          <mesh position={[0, 0, 0.75]}>
-            <boxGeometry args={[0.05, 0.05, 0.5]} />
-            <meshLambertMaterial color="#FFFFFF" />
-          </mesh>
-          <mesh position={[0.5, 0, 1]} rotation={[0, -Math.PI / 2, 0]}>
-            <boxGeometry args={[0.05, 0.05, 1]} />
-            <meshLambertMaterial color="#8A9BA8" />
-          </mesh>
+          {/* 하단 */}
+          <BoxItem
+            color="#EEE"
+            position={{ x: 0, y: 0, z: 0.25 }}
+            size={{ x: 0.05, y: 0.05, z: 0.5 }}
+          />
+          <BoxItem
+            color="#777"
+            position={{ x: 0.5, y: 0, z: 0.5 }}
+            size={{ x: 0.05, y: 0.05, z: 1 }}
+            rotation={{ x: 0, y: -Math.PI / 2, z: 0 }}
+          />
           {/* 기둥 */}
-          <mesh position={[1, 0.25, 1]} rotation={[-Math.PI / 2, 0, 0]}>
-            <boxGeometry args={[0.05, 0.05, 0.5]} />
-            <meshLambertMaterial color="#FFFFFF" />
-          </mesh>
-          <mesh position={[1, 0.5, 1]} rotation={[-Math.PI / 2, 0, 0]}>
-            <boxGeometry args={[0.05, 0.05, 0.5]} />
-            <meshLambertMaterial color="#FFFFFF" />
-          </mesh>
-          <mesh position={[0.5, 0.75, 1]} rotation={[0, -Math.PI / 2, 0]}>
-            <boxGeometry args={[0.05, 0.05, 1]} />
-            <meshLambertMaterial color="#8A9BA8" />
-          </mesh>
+          <BoxItem
+            color="#EEE"
+            position={{ x: 1, y: 0.25, z: 0.5 }}
+            size={{ x: 0.05, y: 0.05, z: 0.5 }}
+            rotation={{ x: -Math.PI / 2, y: 0, z: 0 }}
+          />
+          <BoxItem
+            color="#777"
+            position={{ x: 1, y: 0.75, z: 0.5 }}
+            size={{ x: 0.05, y: 0.05, z: 0.5 }}
+            rotation={{ x: -Math.PI / 2, y: 0, z: 0 }}
+          />
+          {/* 상단 */}
+          <BoxItem
+            color="#EEE"
+            position={{ x: 0.75, y: 1, z: 0.5 }}
+            size={{ x: 0.05, y: 0.05, z: 0.5 }}
+            rotation={{ x: 0, y: -Math.PI / 2, z: 0 }}
+          />
+          <BoxItem
+            color="#777"
+            position={{ x: 0.25, y: 1, z: 0.5 }}
+            size={{ x: 0.05, y: 0.05, z: 0.5 }}
+            rotation={{ x: 0, y: -Math.PI / 2, z: 0 }}
+          />
 
-          <group position={[0.5, 0.75, 1]}>
-            <mesh position={[-0.5, 0.05, -0.05]}>
+          {/* 계단 */}
+          <group position={[0.25, 1, 0.5]}>
+            <mesh position={[-0.25, 0.05, -0.05]}>
               <boxGeometry args={[0.1, 0.05, 0.1]} />
-              <meshLambertMaterial color="#8A9BA8" />
+              <meshLambertMaterial color="#777" />
             </mesh>
-            <mesh position={[-0.5, 0.1, -0.1]}>
+            <mesh position={[-0.25, 0.1, -0.1]}>
               <boxGeometry args={[0.1, 0.05, 0.1]} />
-              <meshLambertMaterial color="#8A9BA8" />
+              <meshLambertMaterial color="#777" />
             </mesh>
-            <mesh position={[-0.5, 0.15, -0.15]}>
+            <mesh position={[-0.25, 0.15, -0.15]}>
               <boxGeometry args={[0.1, 0.05, 0.1]} />
-              <meshLambertMaterial color="#8A9BA8" />
+              <meshLambertMaterial color="#777" />
             </mesh>
-            <mesh position={[-0.5, 0.2, -0.2]}>
+            <mesh position={[-0.25, 0.2, -0.2]}>
               <boxGeometry args={[0.1, 0.05, 0.1]} />
-              <meshLambertMaterial color="#8A9BA8" />
+              <meshLambertMaterial color="#777" />
             </mesh>
-            <mesh position={[-0.5, 0.25, -0.25]}>
+            <mesh position={[-0.25, 0.25, -0.25]}>
               <boxGeometry args={[0.1, 0.05, 0.1]} />
-              <meshLambertMaterial color="#8A9BA8" />
+              <meshLambertMaterial color="#777" />
             </mesh>
-            <mesh position={[-0.5, 0.3, -0.3]}>
+            <mesh position={[-0.25, 0.3, -0.3]}>
               <boxGeometry args={[0.1, 0.05, 0.1]} />
-              <meshLambertMaterial color="#8A9BA8" />
+              <meshLambertMaterial color="#777" />
             </mesh>
-            <mesh position={[-0.5, 0.35, -0.35]}>
+            <mesh position={[-0.25, 0.35, -0.35]}>
               <boxGeometry args={[0.1, 0.05, 0.1]} />
-              <meshLambertMaterial color="#8A9BA8" />
+              <meshLambertMaterial color="#777" />
             </mesh>
-            <mesh position={[-0.5, 0.4, -0.4]}>
+            <mesh position={[-0.25, 0.4, -0.4]}>
               <boxGeometry args={[0.1, 0.05, 0.1]} />
-              <meshLambertMaterial color="#8A9BA8" />
+              <meshLambertMaterial color="#777" />
             </mesh>
-            <mesh position={[-0.5, 0.45, -0.45]}>
+            <mesh position={[-0.25, 0.45, -0.45]}>
               <boxGeometry args={[0.1, 0.05, 0.1]} />
-              <meshLambertMaterial color="#8A9BA8" />
+              <meshLambertMaterial color="#777" />
+            </mesh>
+            <mesh position={[-0.25, 0.45, -0.6]}>
+              <boxGeometry args={[0.2, 0.05, 0.2]} />
+              <meshLambertMaterial color="#777" />
             </mesh>
           </group>
         </group>
 
-        <AxisHelper />
+        {/* <axesHelper /> */}
 
         <OrbitControls
           enablePan={false}
@@ -133,5 +131,37 @@ function MonumentValley() {
         />
       </Canvas>
     </div>
+  );
+}
+
+interface Vector {
+  x: number;
+  y: number;
+  z: number;
+}
+
+function BoxItem({
+  color,
+  position,
+  rotation,
+  size,
+  ...props
+}: Omit<ComponentProps<"mesh">, "position" | "rotation"> & {
+  color: string;
+  position: Vector;
+  rotation?: Vector;
+  size: Vector;
+}) {
+  return (
+    <mesh
+      position={[position.x, position.y, position.z]}
+      rotation={
+        rotation != null ? [rotation.x, rotation.y, rotation.z] : undefined
+      }
+      {...props}
+    >
+      <boxGeometry args={[size.x, size.y, size.z]} />
+      <meshLambertMaterial color={color} />
+    </mesh>
   );
 }
